1731018332eb00ef11d942b5f534f576e01ea7f7
Rosalia Kristanty Manurung <222112339@stis.ac.id>
2025-06-25 09:38:52 +0700

    Remove run in app

--- app.py
+++ app.py
@@ -2133,4 +2133,5 @@ def rebuild_knowledge():
             pass
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    # app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    pass
