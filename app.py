a27cc6206affe3eac21d6e42d7a5dd075ba9fe49
Rosalia Kristanty Manurung <222112339@stis.ac.id>
2025-06-25 11:54:16 +0700

    Modifiy app

--- app.py
+++ app.py
@@ -2133,5 +2133,6 @@ def rebuild_knowledge():
             pass
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True)
+    # app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
     # pass
