da4e91beea038230996cec6c4f816c2b2364e11a
ROSALIA KRISTANTY MANURUNG <222112339@stis.ac.id>
2025-07-04 13:21:55 +0700

    Change delete param

--- app.py
+++ app.py
@@ -1594,7 +1594,7 @@ def admin_delete_file_github(filename):
 
         qdrant_client.delete(
             collection_name=collection_name,
-            filter=delete_filter
+            points_selector=delete_filter
         )
         print(f"[Qdrant] Deleted vectors where source == '{filename}'")
 
